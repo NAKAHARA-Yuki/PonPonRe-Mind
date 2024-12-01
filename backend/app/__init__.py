893341e039341ddeada423c6fa8b174df6096530
NAKAHARA Yuki <nakahara.yuki412@gmail.com>
2024-12-01 17:20:55 +0900

    Mariadb

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
-
-# データベース設定
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@db/flask_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 from app.controllers import main_controller
